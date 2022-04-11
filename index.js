47c2ce455c5791100954a6f03730e3ffba53b5f9
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-11 18:19:35 +0200

    feat(react) : modification des liens localhost vers heroku

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ const movieRoute = require("./routes/movies");
 const listRoute = require("./routes/lists");
 
 dotenv.config();
+const path = require('path');
+
+SECRET_KEY = 'netflix';
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("DB Connection Successfull"))
-  .catch((err) => {
-    console.error(err);
-  });
+	.connect(
+		'mongodb+srv://netflix:netflix@netflix.fp9rf.mongodb.net/netflix?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		},
+	)
+	.then(() => console.log('DB Connection Successfull'))
+	.catch(err => {
+		console.error(err);
+	});
 
 app.use(express.json());
 
@@ -27,6 +33,12 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running!");
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log('Backend server is running!');
 });
